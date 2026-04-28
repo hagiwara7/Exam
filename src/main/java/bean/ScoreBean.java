5b39db1a60ac6bba6f5973b2c90c1823535e4534
こやのまお <doragonnnann1@gmail.com>
2026-04-28 11:26:33 +0900

    Add ScoreBean class with student score attributes

--- src/main/java/bean/ScoreBean.java
+++ src/main/java/bean/ScoreBean.java
@@ -1,1 +1,60 @@
+package bean;
 
+public class ScoreBean {
+
+    private String studentId;
+    private String schoolCode;
+    private String subjectCode;
+    private int examCount;
+    private int score;
+    private String classNum;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getSchoolCode() {
+        return schoolCode;
+    }
+
+    public void setSchoolCode(String schoolCode) {
+        this.schoolCode = schoolCode;
+    }
+
+    public String getSubjectCode() {
+        return subjectCode;
+    }
+
+    public void setSubjectCode(String subjectCode) {
+        this.subjectCode = subjectCode;
+    }
+
+    public int getExamCount() {
+        return examCount;
+    }
+
+    public void setExamCount(int examCount) {
+        this.examCount = examCount;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    public String getClassNum() {
+        return classNum;
+    }
+
+    public void setClassNum(String classNum) {
+        this.classNum = classNum;
+    }
+}
+``
