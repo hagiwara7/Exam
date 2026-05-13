e413e804261499a8cef0d0d2a356cd05c28da497
yukizane1012 <oom2525096@stu.o-hara.ac.jp>
2026-05-13 14:20:42 +0900

    Update SubjectUpdateExecuteAction.java
    科目変更(変更)　最強

--- src/main/java/scoremanager/main/SubjectUpdateExecuteAction.java
+++ src/main/java/scoremanager/main/SubjectUpdateExecuteAction.java
@@ -1,4 +1,8 @@
 package scoremanager.main;
+import java.util.HashMap;
+import java.util.Map;
+
+import bean.School;
 import bean.Subject;
 import bean.Teacher;
 import dao.SubjectDao;
@@ -6,32 +10,46 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.servlet.http.HttpSession;
 import tool.Action;
- 
+
 public class SubjectUpdateExecuteAction extends Action {
 	@Override
 	public void execute(HttpServletRequest req, HttpServletResponse res) throws Exception {
 		
 		HttpSession session = req.getSession();
 		Teacher teacher = (Teacher) session.getAttribute("user");
- 
+		Map<String, String> errors = new HashMap<>();
+		
 		String cd=""; // 科目コード
 		String name=""; // 科目名
 		
-		Subject subject=new Subject();
 		SubjectDao subjectDao=new SubjectDao(); // 科目の情報をDBから取得するのに必要
 		
 		//リクエストパラメータから取得
 		cd=req.getParameter("cd");
 		name=req.getParameter("name");
 		
-		//subjectに変更する科目の情報をセット
-		subject.setCd(cd);
-		subject.setName(name);
-		subject.setSchool(teacher.getSchool());
+        School school = teacher.getSchool();
+        
+        //科目変更中に変更対象が削除された際の処理
+        if (subjectDao.get(cd, school) == null) {
+        	errors.put("1", "科目情報が存在しません");//エラー文をセット
+     
+    	    req.setAttribute("errors", errors);
+    		req.setAttribute("cd", cd);
+    		req.setAttribute("name", name);
+    		req.getRequestDispatcher("SubjectUpdate.action").forward(req, res);//前のサーブレットに戻る
+        }
+        
+      //subjectに変更する科目の情報をセット
+		 Subject subject=new Subject();
+		 subject.setCd(cd);
+		 subject.setName(name);
+		 subject.setSchool(teacher.getSchool());
 		
-		// DBに変更内容を保存
-		subjectDao.save(subject);
-		// JSPへフォワード 7
-		req.getRequestDispatcher("subject_update_done.jsp").forward(req, res);
+	  // DBに変更内容を保存
+		 subjectDao.save(subject);
+		 
+	  // jspへフォワード
+		 req.getRequestDispatcher("subject_update_done.jsp").forward(req, res);
 	}
 }
