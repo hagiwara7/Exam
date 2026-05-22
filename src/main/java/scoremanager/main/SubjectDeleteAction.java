de612537160b4c1de77573c1b7c3735af36bceb7
yukizane1012 <oom2525096@stu.o-hara.ac.jp>
2026-05-22 11:22:49 +0900

    Update SubjectDeleteAction.java

--- src/main/java/scoremanager/main/SubjectDeleteAction.java
+++ src/main/java/scoremanager/main/SubjectDeleteAction.java
@@ -15,19 +15,19 @@ public class SubjectDeleteAction extends Action {
 		HttpSession session = req.getSession(); // セッション
 		Teacher teacher = (Teacher)session.getAttribute("user");
 		
-		//
+		//科目番号と科目名を取得
 		String cd = req.getParameter("subject_cd");
 		String name = req.getParameter("subject_name");
+
 		
 		SubjectDao subjectDao=new SubjectDao();
-		
+		//科目番号と教員情報から科目情報を取得
 		Subject subject = subjectDao.get(cd, teacher.getSchool());
+
 		
-		
-		//
 		req.setAttribute("subject",subject);
 		
-		// フォワード
+		// JSPへフォワード
 		req.getRequestDispatcher("subject_delete.jsp").forward(req, res);
 
 	}
